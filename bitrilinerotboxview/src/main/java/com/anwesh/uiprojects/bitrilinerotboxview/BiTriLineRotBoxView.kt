ad13a258073e3a7a1bf6d1afa5c0efdfc1bca9c3
Anwesh43 <anweshthecool0@gmail.com>
2020-10-01 15:58:43 +0530

    adding Canvas extension methods

--- bitrilinerotboxview/src/main/java/com/anwesh/uiprojects/bitrilinerotboxview/BiTriLineRotBoxView.kt
+++ bitrilinerotboxview/src/main/java/com/anwesh/uiprojects/bitrilinerotboxview/BiTriLineRotBoxView.kt
@@ -32,3 +32,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBiTriangleRotBox(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val hSize : Float = size / hFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    rotate(180f * sf4)
+    scale(sf1, sf1)
+    drawRect(RectF(-size, -hSize, size, hSize), paint)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        drawLine(-size, -hSize, -size + size * sf2, -hSize + 2 * hSize * sf1, paint)
+        drawLine(0f, -hSize, size * sf3, -hSize + hSize * sf3, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBTLRBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiTriangleRotBox(scale, w, h, paint)
+}
