95669c9a9ed4daa1a9e513f544eff52ad5700f5a
Anwesh43 <anweshthecool0@gmail.com>
2020-10-02 07:08:33 +0530

    adding Canvas extension methods

--- frombottomtrilineview/src/main/java/com/anwesh/uiprojects/frombottomtrilineview/FromBottomLineView.kt
+++ frombottomtrilineview/src/main/java/com/anwesh/uiprojects/frombottomtrilineview/FromBottomLineView.kt
@@ -30,3 +30,26 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawFromBottomLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val hSize : Float = h / hFactor
+    save()
+    translate(0f, h)
+    drawLine(0f, 0f, (w / 2) * sf1, -hSize * sf1, paint)
+    drawLine(w / 2, -hSize, w / 2, -hSize - (h - hSize) * sf2, paint)
+    drawLine(w / 2, -hSize, w / 2 + (w / 2) * sf3, -hSize + hSize * sf3, paint)
+    restore()
+}
+
+fun Canvas.drawFBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawFromBottomLine(scale, w, h, paint)
+}
