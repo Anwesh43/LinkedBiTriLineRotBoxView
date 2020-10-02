8187d9f39ef31df93c9a3c249d604aa776856267
Anwesh43 <anweshthecool0@gmail.com>
2020-10-02 07:12:22 +0530

    adding FromBottomLineView class

--- frombottomtrilineview/src/main/java/com/anwesh/uiprojects/frombottomtrilineview/FromBottomLineView.kt
+++ frombottomtrilineview/src/main/java/com/anwesh/uiprojects/frombottomtrilineview/FromBottomLineView.kt
@@ -53,3 +53,19 @@ fun Canvas.drawFBLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawFromBottomLine(scale, w, h, paint)
 }
+
+class FromBottomLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
