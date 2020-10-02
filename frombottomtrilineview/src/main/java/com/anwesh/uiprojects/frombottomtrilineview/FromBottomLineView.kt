c8d9283cbe5a59983297dea0627b5987f4072956
Anwesh43 <anweshthecool0@gmail.com>
2020-10-02 07:23:04 +0530

    adding FBLNode class

--- frombottomtrilineview/src/main/java/com/anwesh/uiprojects/frombottomtrilineview/FromBottomLineView.kt
+++ frombottomtrilineview/src/main/java/com/anwesh/uiprojects/frombottomtrilineview/FromBottomLineView.kt
@@ -116,4 +116,45 @@ class FromBottomLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class FBLNode(var i : Int, val state : State = State()) {
+
+        private var next : FBLNode? = null
+        private var prev : FBLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = FBLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFBLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FBLNode {
+            var curr : FBLNode? = next
+            if (dir == 1) {
+                curr = prev
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
